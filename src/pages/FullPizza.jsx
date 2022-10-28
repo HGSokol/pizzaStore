d5a26a2a473adfa67d9190a8bac31dafb0139620
Соколов Глеб <g.sokolov@21vek.local>
2022-10-28 09:23:42 +0300

    add outlet

--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -1,48 +1,51 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
-import { useParams, Link } from "react-router-dom"
+import { useParams, Link, useNavigate } from "react-router-dom"
 
 export const FullPizza = () => {
-  const [items, setItems] = useState(null)
+  const [item, setItem] = useState(null)
   const { id } = useParams()
+  const navigate = useNavigate()
 
   useEffect(() => {
-    const fetch = async() => {
-      const {data} = await axios.get(`https://634846130484786c6e965029.mockapi.io/items/${id}`)
-
-      setItems(data)
-      console.log(data)
+    const fetch = async () => {
+      try{
+        const { data } = await axios.get(`https://634846130484786c6e965029.mockapi.io/items/${id}`)
+        setItem(data)
+      } catch(e){
+        alert('Ошибка получения пицц!')
+        navigate('/')
+      }
     }
-    fetch()
 
+    fetch()
   },[])
   
-  if(items) {
-    const {price, title, imageUrl, ingredients} = items
-
-    return (
-      <div className='container'>
-          <div className='singlePizza'>
-            <img src={imageUrl} alt="Pizza"/>
-            <div className='singlePizza__title'>
-              <h2>{title}</h2>
-              <p>{ingredients}</p>
-              <div className="pizza-block__bottom">
-                <div className="pizza-block__price">{`от ${price} ₽`}</div>
-              </div>
-              <Link to='/'>
-                <button 
-                  className="button button--black">
-                  <span>
-                    Вернуться назад
-                  </span>
-                </button>
-              </Link>
-            </div>
-          </div>
-      </div>
-    )
+  if(!item) {
+    return <div>Загрузка...</div>
   }
 
-  return <div> Скелетон </div>
+  const {price, title, imageUrl, ingredients} = item
+  return (
+    <div className='container'>
+        <div className='singlePizza'>
+          <img src={imageUrl} alt="Pizza"/>
+          <div className='singlePizza__title'>
+            <h2>{title}</h2>
+            <p>{ingredients}</p>
+            <div className="pizza-block__bottom">
+              <div className="pizza-block__price">{`от ${price} ₽`}</div>
+            </div>
+            <Link to='/'>
+              <button 
+                className="button button--black">
+                <span>
+                  Вернуться назад
+                </span>
+              </button>
+            </Link>
+          </div>
+        </div>
+    </div>
+  )
 }
